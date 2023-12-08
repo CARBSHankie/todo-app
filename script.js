143e95c183ef4f9dfa4597494b68f2e43b396a57
Hankui Wang <60219052+CARBSHankie@users.noreply.github.com>
2023-12-08 14:04:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-console.log("JavaScript is working!");
+const backendUrl = 'https://todo-app-backend-wangroger93.replit.app';
 
-// Fetch tasks from backend and display them
-function fetchTasks() {
-    fetch('https://todo-app-backend-wangroger93.repl.co/tasks')
-        .then(response => response.json())
-        .then(tasks => {
-            const tasksList = document.getElementById('todoList');
-            tasksList.innerHTML = ''; // Clear existing tasks
-
-            tasks.forEach(task => {
-                let listItem = document.createElement('li');
-                listItem.textContent = task.text; // Assuming each task has 'text' property
-                tasksList.appendChild(listItem);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-// Add a new task and send it to the backend
-function addTask() {
-    let input = document.getElementById('todoInput');
-    let newTaskText = input.value;
-    if (newTaskText !== '') {
-        fetch('https://todo-app-backend-wangroger93.replit.app', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ text: newTaskText })
-        })
-        .then(response => response.json())
-        .then(newTask => {
-            let listItem = document.createElement('li');
-            listItem.textContent = newTask.text;
-            document.getElementById('todoList').appendChild(listItem);
-            input.value = ''; // Clear the input
-        })
-        .catch(error => console.error('Error:', error));
+async function fetchTodos() {
+    try {
+        const response = await fetch(`${backendUrl}/todos`);
+        const todos = await response.json();
+        displayTodos(todos);
+    } catch (error) {
+        console.error('Error fetching todos:', error);
     }
 }
 
+function displayTodos(todos) {
+    const list = document.getElementById('todo-list');
+    list.innerHTML = '';
+    todos.forEach(todo => {
+        const item = document.createElement('li');
+        item.textContent = todo.task;
+        list.appendChild(item);
+    });
+}
 
-// Call fetchTasks on page load to display existing tasks
-window.onload = function() {
-    fetchTasks();
-};
+async function addTodo() {
+    const input = document.getElementById('new-todo');
+    const task = input.value.trim();
+    if (!task) return;
+
+    try {
+        await fetch(`${backendUrl}/todos`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ task }),
+        });
+        input.value = '';
+        fetchTodos();
+    } catch (error) {
+        console.error('Error adding todo:', error);
+    }
+}
+
+// Initial fetch of todos
+fetchTodos();
