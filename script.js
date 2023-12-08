fa1c785b60103f4e0c30df17691458c3aa572c17
Hankui Wang <60219052+CARBSHankie@users.noreply.github.com>
2023-12-08 11:57:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 console.log("JavaScript is working!");
 
+// Fetch tasks from backend and display them
+function fetchTasks() {
+    fetch('https://todo-app-backend--wangroger93.repl.co/tasks')
+        .then(response => response.json())
+        .then(tasks => {
+            const tasksList = document.getElementById('todoList');
+            tasksList.innerHTML = ''; // Clear existing tasks
+
+            tasks.forEach(task => {
+                let listItem = document.createElement('li');
+                listItem.textContent = task.text; // Assuming each task has 'text' property
+                tasksList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// Add a new task and send it to the backend
 function addTask() {
     let input = document.getElementById('todoInput');
-    let newTask = input.value;
-    if (newTask !== '') {
-        let listItem = document.createElement('li');
-        listItem.textContent = newTask;
-        document.getElementById('todoList').appendChild(listItem);
-        input.value = ''; // Clear the input
+    let newTaskText = input.value;
+    if (newTaskText !== '') {
+        fetch('https://todo-app-backend--wangroger93.repl.co/tasks', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text: newTaskText }) // Update as per your backend's expected format
+        })
+        .then(response => response.json())
+        .then(newTask => {
+            let listItem = document.createElement('li');
+            listItem.textContent = newTask.text; // Assuming the response includes the new task
+            document.getElementById('todoList').appendChild(listItem);
+            input.value = ''; // Clear the input
+        })
+        .catch(error => console.error('Error:', error));
     }
 }
 
-function fetchTasks() {
-    fetch('https://todo-app-backend-wangroger93.replit.app/tasks')
-        .then(response => response.json())
-        .then(tasks => {
-            // process and display tasks
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
+// Call fetchTasks on page load to display existing tasks
+window.onload = function() {
+    fetchTasks();
+};
