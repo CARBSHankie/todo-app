96000d950164b0547913fd7aa207e1c37468d509
Hankui Wang <60219052+CARBSHankie@users.noreply.github.com>
2023-12-08 11:22:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function addTask() {
         input.value = ''; // Clear the input
     }
 }
+
+function fetchTasks() {
+    fetch('https://todo-app-backend--wangroger93.repl.co/tasks')
+        .then(response => response.json())
+        .then(tasks => {
+            // Process and display the tasks
+        })
+        .catch(error => console.error('Error:', error));
+}
